0efd96eaef801877a9eadffd37fe0eb832bc500a
Mariam <mbadalyan3@gmail.com>
2023-09-20 12:21:26 +0200

    add Income function

--- src/Income.jsx
+++ src/Income.jsx
@@ -1,35 +1,44 @@
-// import { useId, useState } from "react";
+import React, { useCallback, useState } from "react";
 
 export const Income = () => {
-  const btn = document.getElementById("count");
-  if (btn) {
-    btn.onclick = function () {
-      const itax = document.getElementById("input-salary").value;
-      console.log(itax);
-      return Number;
-    };
-  }
+  const [salary, changeSalary] = useState(0);
+  const [tax, changeTax] = useState(0);
+
+  const onSalaryChange = useCallback((e) => {
+    changeSalary(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    const newTax = calculateNetMonthIncome(salary);
+
+    changeTax(newTax);
+  }, [salary]);
 
   return (
     <div className="wrapper-tax">
       <div className="tax-wrp">
         <p>Salary for the year</p>
-        <input type="number" min="0" autoFocus required id="input-salary" />
+        <input
+          onChange={onSalaryChange}
+          value={salary}
+          type="number"
+          min="0"
+          autoFocus
+          required
+          id="input-salary"
+        />
       </div>
       <div className="tax-wrp">
         <p>How much taxes do you have to pay in a year</p>
-        <div id="income-tax"></div>
+        <div id="income-tax">{tax === 0 ? null : tax}</div>
       </div>
-      <button className="count" id="count">
+      <button onClick={onSubmit} className="count" id="count">
         count
       </button>
     </div>
   );
 };
 
-// const sum = document.getElementById("income-tax")
-
-//---------------------------------------------------------------------
 const taxIntervals = [
   { maxValue: 35129, rate: 9.45 },
   { maxValue: 68507, rate: 37.1 },
@@ -64,17 +73,3 @@ export function calculateNetMonthIncome(yearIncome) {
 // 1 isFinalInterval = false -> currentTaxableIncome = 35129 -> (35129 - 0) * 9.45 -> +tax
 // 2 isFinalInterval = false -> currentTaxableIncome = 68507 -> (68507 - 35129) * 37.1 -> +tax
 // 3 isFinalInterval = true -> currentTaxableIncome = 80000 -> (80000 - 68507) * 49.5 -> +tax
-
-//const btn = document.getElementById("count");
-// if (btn) {
-//   btn.onclick = function () {
-//     var _a;
-//     const itax = document.getElementById("input-salary");
-//     const value =
-//       (_a = itax === null || itax === void 0 ? void 0 : itax.value) !== null &&
-//       _a !== void 0
-//         ? _a
-//         : 0;
-//     console.info(value);
-//   };
-//}
